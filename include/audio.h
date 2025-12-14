7db49ccde22c96e67ae64c50320cffbb80a9682d
SitiSuhaira <sitisuhaira761@gmail.com>
2025-12-14 23:24:11 +0700

    Create audio.h

---
+++ include/audio.h
@@ -0,0 +1,14 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+bool initAudio();
+void playBGM();
+void playMenuBGM();
+void stopBGM();
+void sfxShoot();
+void sfxExplosion();
+void sfxGameOver();
+void sfxWave();
+void closeAudio();
+
+#endif
